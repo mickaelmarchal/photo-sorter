3f6619a4e6a7b239565243b3e9a8167ebde99f86
Mickael Marchal <mickaelmarchal@users.noreply.github.com>
2018-05-23 13:31:33 +0200

    feat: improved error management

--- photos.js
+++ photos.js
@@ -7,7 +7,7 @@ var ExifImage = require('exif').ExifImage;
 
 // Get base directory from args - use current dir if not specified
 var baseDir = '.';
-if(typeof process.argv[2] != 'undefined') {
+if (typeof process.argv[2] != 'undefined') {
   baseDir = process.argv[2];
 }
 
@@ -15,11 +15,11 @@ if(typeof process.argv[2] != 'undefined') {
  * Read exif data for given file
  * If file is not a picture, return an exception
  */
-var getPhotoTakenDate = function(filename) {
+var getPhotoTakenDate = function (filename) {
 
   var deferred = q.defer();
 
-  new ExifImage({image: baseDir + '/' + filename}, function (error, exifData) {
+  new ExifImage({ image: baseDir + '/' + filename }, function (error, exifData) {
     if (error) {
       deferred.reject(error);
     } else {
@@ -34,7 +34,7 @@ var getPhotoTakenDate = function(filename) {
           "$1-$2-$3T$4:$5:$6"
         ));
 
-        if (! date) {
+        if (!date) {
           deferred.reject('Date not valid');
         }
 
@@ -43,7 +43,7 @@ var getPhotoTakenDate = function(filename) {
           takenDate: date
         });
 
-      } catch(e) {
+      } catch (e) {
         deferred.reject('Unable to parse date "' + exifData.exif.DateTimeOriginal + '" in EXIF data for file ' + filename);
       }
     }
@@ -57,66 +57,99 @@ var getPhotoTakenDate = function(filename) {
  * Move the given file to a directory corresponding to given date
  * (directory is created if not exists)
  */
-var moveToDateDirectory = function(filename, date) {
+var moveToDateDirectory = function (filename, date) {
 
   var deferred = q.defer();
-  var dirName = date.toISOString().substring(0, 10);
+
+  if (date instanceof Date) {
+    var dirName = date.toISOString().substring(0, 10);
+  } else if (typeof date == 'string') {
+    dirName = date;
+  }
 
   // create directory (if not existing)
-  mkdirp(baseDir + '/' + dirName, function(error) {
+  mkdirp(baseDir + '/' + dirName, function (error) {
 
-      if(error) {
-        deferred.reject('Could not create / access directory "' + baseDir + '/' + dirName + '": ' + error);
+    if (error) {
+      deferred.reject('Could not create / access directory "' + baseDir + '/' + dirName + '": ' + error);
+    }
+
+    // move file to directory
+    fs.rename(baseDir + '/' + filename, baseDir + '/' + dirName + '/' + filename, function (error) {
+      if (error) {
+        deferred.reject('Could not move file "' + filename + '" to directory "' + baseDir + '/' + dirName + '"');
+      } else {
+        deferred.resolve({
+          dirName: dirName,
+          filename: filename
+        });
       }
-
-      // move file to directory
-      fs.rename(baseDir + '/' + filename, baseDir + '/' + dirName + '/' + filename, function(error) {
-        if(error) {
-          deferred.reject('Could not move file "' + filename + '" to directory "' + baseDir + '/' + dirName + '"');
-        } else {
-          deferred.resolve({
-            dirName: dirName,
-            filename: filename
-          });
-        }
-      });
+    });
 
   });
 
   return deferred.promise;
 };
 
-
-
-
-fs.readdir(baseDir, function(error, files) {
-  if (! error) {
+/* *
+// 1. read all jpegs, take exif data, use it to sort pictures
+fs.readdir(baseDir, function (error, files) {
+  if (!error) {
 
     // Find images and read exif data
-    for(var i = 0; i < files.length; i++) {
-      if(files[i].match(/\.(jpg|jpeg)$/i)) {
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].match(/\.(jpg|jpeg)$/i)) {
 
         getPhotoTakenDate(files[i]).then(
-          function(data) {
+          function (data) {
             moveToDateDirectory(data.filename, data.takenDate).then(
-              function(data) {
+              function (data) {
                 console.log('File "' + data.filename + '" moved to directory "' + data.dirName + '"');
               },
-              function(error) {
+              function (error) {
                 console.log(error);
               }
             );
           },
-          function(error) {
+          function (error) {
             console.log(error);
           })
-        ;
+          ;
 
 
       }
     }
-
   } else {
     throw error;
   }
 });
+/* */
+
+/* */
+//2. read files, find date in filename, use it to sort pictures
+fs.readdir(baseDir, function (error, files) {
+  if (!error) {
+
+    // Find images and read exif data
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].match(/\.(mp4|jpg|jpeg)$/i)) {
+
+        var filename = files[i];
+        if (filename.match(/^[0-9]{8}_/)) {
+          var date = filename.substr(0, 4) + '-' + filename.substr(4, 2) + '-' + filename.substr(6, 2);
+          moveToDateDirectory(filename, date).then(
+            function (data) {
+              console.log('File "' + filename + '" moved to directory');
+            },
+            function (error) {
+              console.log(error);
+            }
+          );
+        }
+      }
+    }
+  } else {
+    throw error;
+  }
+});
+/* */
